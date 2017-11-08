7a0d49a0418786bbcf4fdc2d9fe979d901605287
elliottpiercy <elliottpiercy@hotmail.co.uk>
2017-11-08 13:56:29 +0000

    Update motors.py

--- src/motors.py
+++ src/motors.py
@@ -57,7 +57,45 @@ class Motors:
 				end = time.time()
 			self.go()
 
+	def measure(self,distance):
+		
 
+        	distance = 0.5
+       		filterLeft = 1.0
+        	filterRight = 0.7
+
+
+       		self.IO.setMotors(60*filterLeft , 60*filterRight)
+
+        	t = distance/0.24   # equation is y = 22.8x but using 24 take rolling into account
+
+
+        	end = time.time()
+
+
+        	rotations = distance / 0.103044239
+
+        	start = time.time()
+
+        	while rotations > 0 and (end-start <t):  #if there are still rotations left and there is time still left
+          		end = time.time()
+
+
+            	digital = self.IO.getInputs()
+            	if digital[7] :        #if the hall effect value is true
+                	rotations = rotations - 1   # take one rotation off
+            	if rotations < 1 and (end-start) < t:
+                	print("Stopped at break point")
+                	break
+            	while digital[7]:
+                	pass
+
+
+        	print("Done")
+        	self.IO.setMotors(0,0)
+
+        
+		
 	def turnUntil(self, direction, callback, InterruptExecution, *param):
 		"""
 		Turn until callback function raises exception.
