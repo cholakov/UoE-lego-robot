74ee7afa37b925d2df0061ca99f78ba295be7c70
elliottpiercy <elliottpiercy@hotmail.co.uk>
2017-11-08 14:42:00 +0000

    Update sensors.py

--- src/sensors.py
+++ src/sensors.py
@@ -4,25 +4,41 @@ class Hall():
 		self.DEFINITION = 17.8 # centimeters covered between two signals from the hall effect
 
 	def measure(self, distance):
-		"""
-		Measure distance
-		:param distance: in cm how much distance you want to cover
-		:return:
-		"""
-		# 5.6 signals from the hall effect per meter, or 17.8 cm per signal
+		
+        distance = 0.5
+        filterLeft = 1.0
+        filterRight = 0.7
 
-		# rotations = (distance / (2*np.pi*0.041))      #radius of the wheel = 4.1cm = 0.041m, gearRatio = 0.4
-		# rotations = rotations / 0.4
-		# rotations = (2*rotations) /3
 
-		rotations = distance / self.DEFINITION
+        self.IO.setMotors(60*filterLeft , 60*filterRight)
 
-		while rotations > 0 :
-			digital = self.IO.getInputs()
-			if digital[7] :        #if the hall effect value is true
-				rotations = rotations - 1   # take one rotation off
-			while digital[7]:
-				pass
+        t = distance/0.24   # equation is y = 22.8x but using 24 take rolling into account
+
+
+        
+
+
+        rotations = distance / 0.103044239
+
+        start = time.time()
+	end = time.time()
+
+        while rotations > 0 and (end-start <t):  #if there are still rotations left and there is time still left
+            end = time.time()
+
+
+            digital = self.IO.getInputs()
+            if digital[7] :        #if the hall effect value is true
+                rotations = rotations - 1   # take one rotation off
+            if rotations < 1 and (end-start) < t:
+                print("Stopped at break point")
+                break
+            while digital[7]:
+                pass
+
+
+        print("Done 0.5m")
+        self.IO.setMotors(0,0)
 
 class Sensors():
 	def __init__(self, IO):
