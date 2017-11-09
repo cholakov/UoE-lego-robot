c170d8e172fc98fb3d0dc1edf72194ecf984a1c3
elliottpiercy <elliottpiercy@hotmail.co.uk>
2017-11-09 18:57:22 +0000

    Eddited measure()

--- src/sensors.py
+++ src/sensors.py
@@ -6,40 +6,40 @@ class Hall():
 		
 	def measure(self, distance):
 		
-        distance = 0.5
-        filterLeft = 1.0
-        filterRight = 0.7
+        	self.go()
+		
+        	filterLeft = 1.0
+        	filterRight = 0.7
 
 
-        self.IO.setMotors(60*filterLeft , 60*filterRight)
+        	self.IO.setMotors(60*filterLeft , 60*filterRight)
 
-        t = distance/0.24   # equation is y = 22.8x but using 24 take rolling into account
+        	t = distance/0.24   # equation is y = 22.8x but using 24 take rolling into account
 
 
         
 
 
-        rotations = distance / 0.103044239
+        	rotations = distance / 0.103044239
 
-        start = time.time()
-	end = time.time()
+        	start = time.time()
+		end = time.time()
 
-        while rotations > 0 and (end-start <t):  #if there are still rotations left and there is time still left
-            end = time.time()
-
-
-            digital = self.IO.getInputs()
-            if digital[7] :        #if the hall effect value is true
-                rotations = rotations - 1   # take one rotation off
-            if rotations < 1 and (end-start) < t:
-                print("Stopped at break point")
-                break
-            while digital[7]:
-                pass
+        	while rotations > 0 and (end-start <t):  #if there are still rotations left and there is time still left
+          		end = time.time()
 
 
-        print("Done 0.5m")
-        self.IO.setMotors(0,0)
+            	digital = self.IO.getInputs()
+            	if digital[7] :        #if the hall effect value is true
+                	rotations = rotations - 1   # take one rotation off
+            	if rotations < 1 and (end-start) < t:
+                	print("Stopped at break point")
+                	break
+            	while digital[7]:
+                	pass
+
+
+        self.stop()
 
 class Sensors():
 	def __init__(self, IO):
